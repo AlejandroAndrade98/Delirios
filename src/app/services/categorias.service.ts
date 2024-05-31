9c635bdd78df94a1dc3ccaf1b32f587b459eb4db
Alejandro Andrade <alejandro21112@hotmail.com>
2024-05-31 11:36:58 -0500

    ng build para la pagina de github pages

--- src/app/services/categorias.service.ts
+++ src/app/services/categorias.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-// import { Producto } from '../models/producto';
 import { apiServer } from '../components/apiServer';
 import { Categorias } from '../models/categorias';
-import { TestBed } from '@angular/core/testing';
 
 @Injectable({
   providedIn: 'root'
@@ -19,11 +17,4 @@ export class CategoriasService {
       return this.http.get<Categorias[]>(this.CategoriasUrl);
     }
   
-  }
-beforeEach(() => {
-    TestBed.configureTestingModule({
-        imports: [HttpClientModule],
-        providers: [CategoriasService],
-    });
-   
-});
+}
